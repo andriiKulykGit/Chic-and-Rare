2c607fe5ae67539088848c9301170d271fa08815
Andrii <andrii.kulyk.job@gmail.com>
2024-12-22 23:57:40 +0100

    Fix product card

--- src/js/modules/pop-up.js
+++ src/js/modules/pop-up.js
@@ -2,66 +2,180 @@ import gsap from "gsap";
 import { blockScroll, allowScroll } from "./scroll";
 
 function initPopUps() {
-	sideMenu();
+	/* sideMenu();
+	pickSize(); */
+
+	const IDs = ["cart", "pick-size"];
+
+	IDs.forEach((id) => {
+		const popUp = document.querySelector(`[data-popup="${id}"]`);
+		const closeBtns = popUp.querySelectorAll(`[data-popup-close="${id}"]`);
+		const openBtns = document.querySelectorAll(`[data-popup-open="${id}"]`);
+
+		console.log(openBtns);
+
+
+		openBtns.forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				e.preventDefault();
+				blockScroll();
+				showPopUp(popUp);
+				switch (id) {
+					case "cart":
+						gsap.fromTo(
+							".cart",
+							{
+								borderRadius: "50% 0 0 50%",
+								x: "100%",
+								opacity: 0,
+							},
+							{
+								borderRadius: 0,
+								x: 0,
+								opacity: 1,
+							},
+						);
+						break;
+					case "pick-size":
+						gsap.fromTo(
+							".pick-size",
+							{
+								y: "100%",
+								opacity: 0,
+							},
+							{
+								y: 0,
+								opacity: 1,
+							},
+						);
+						break;
+					default:
+						break;
+				}
+			});
+		});
+
+		closeBtns.forEach((btn) => {
+			btn.addEventListener("click", (e) => {
+				e.preventDefault();
+				hidePopUp(popUp);
+				switch (id) {
+					case "cart":
+						gsap.to(".cart", {
+							borderRadius: "50% 0 0 50%",
+							x: "100%",
+							opacity: 0,
+						});
+						break;
+					case "pick-size":
+						gsap.to(".pick-size", {
+							y: "100%",
+							opacity: 0,
+						});
+						break;
+					default:
+						break;
+				}
+			});
+		});
+	});
 }
 
 function sideMenu() {
 	const sideMenu = document.querySelector(".pop-up_side-menu");
-	const sideMenuBtn = sideMenu.querySelectorAll('[data-popup-close="cart"]');
-	const sideMenuOpenBtn = document.querySelector('[data-popup-taget="cart"]');
+	const closeBtns = sideMenu.querySelectorAll('[data-popup-close="cart"]');
+	const openBtns = document.querySelectorAll('[data-popup-taget="cart"]');
 
-	sideMenuOpenBtn.addEventListener("click", () => {
-		blockScroll();
-
-		gsap.set(".pop-up", {
-            display: "flex",
-            opacity: 1
+	openBtns.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			blockScroll();
+			showPopUp(sideMenu);
+			gsap.fromTo(
+				".cart",
+				{
+					x: "100%",
+					opacity: 0,
+				},
+				{
+					x: 0,
+					opacity: 1,
+				},
+			);
 		});
-        gsap.to('.pop-up__bg', {
-            opacity: 1
-        })
-		gsap.fromTo(
-			".cart",
-			{
-				borderRadius: "50% 0 0 50%",
-				x: "100%",
-				opacity: 0,
-			},
-			{
-				borderRadius: 0,
-				x: 0,
-				opacity: 1,
-			},
-		);
-
 	});
 
-	sideMenuBtn.forEach((btn) => {
+	closeBtns.forEach((btn) => {
 		btn.addEventListener("click", () => {
-			console.log(1);
-
+			hidePopUp(sideMenu);
 			gsap.to(".cart", {
 				borderRadius: "50% 0 0 50%",
 				x: "100%",
 				opacity: 0,
 			});
-            gsap.to('.pop-up__bg', {
-                opacity: 0
-            })
-			gsap.to(".pop-up", {
-				opacity: 0,
-				onComplete: () => {
-					allowScroll();
-					document
-						.querySelector(".pop-up")
-						.classList.remove("pop-up_active");
-                        gsap.set(".pop-up", {
-                            display: "none"
-                        })
-				},
-			});
-
 		});
+	});
+}
+
+function pickSize() {
+	const pickSize = document.querySelector("[data-popup='pick-size']");
+	const closeBtns = pickSize.querySelectorAll(
+		'[data-popup-close="pick-size"]',
+	);
+	const openBtns = document.querySelectorAll('[data-popup-open="pick-size"]');
+
+	openBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			e.preventDefault();
+			blockScroll();
+			showPopUp(pickSize);
+			gsap.fromTo(
+				".pick-size",
+				{
+					y: "100%",
+					opacity: 0,
+				},
+				{
+					y: 0,
+					opacity: 1,
+				},
+			);
+		});
+	});
+
+	closeBtns.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			hidePopUp(pickSize);
+			gsap.to(".pick-size", {
+				y: "100%",
+				opacity: 0,
+			});
+		});
+	});
+}
+
+function showPopUp(elm) {
+	gsap.set(elm, {
+		display: "flex",
+		opacity: 1,
+	});
+	gsap.to(elm.querySelector(".pop-up__bg"), {
+		opacity: 1,
+	});
+}
+
+function hidePopUp(elm) {
+	gsap.to(elm.querySelector(".pop-up__bg"), {
+		opacity: 0,
+	});
+	gsap.to(elm, {
+		opacity: 0,
+		onComplete: () => {
+			allowScroll();
+			elm.classList.remove("pop-up_active");
+			gsap.set(elm, {
+				display: "none",
+			});
+		},
 	});
 }
 
