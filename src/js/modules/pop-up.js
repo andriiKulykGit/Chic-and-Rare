71d04dbac75ac1d372923fa6bea253e0ced343d9
Andrii <andrii.kulyk.job@gmail.com>
2024-12-23 00:01:36 +0100

    Fix product card

--- src/js/modules/pop-up.js
+++ src/js/modules/pop-up.js
@@ -2,18 +2,12 @@ import gsap from "gsap";
 import { blockScroll, allowScroll } from "./scroll";
 
 function initPopUps() {
-	/* sideMenu();
-	pickSize(); */
-
 	const IDs = ["cart", "pick-size"];
 
 	IDs.forEach((id) => {
 		const popUp = document.querySelector(`[data-popup="${id}"]`);
 		const closeBtns = popUp.querySelectorAll(`[data-popup-close="${id}"]`);
 		const openBtns = document.querySelectorAll(`[data-popup-open="${id}"]`);
-
-		console.log(openBtns);
-
 
 		openBtns.forEach((btn) => {
 			btn.addEventListener("click", (e) => {
@@ -76,78 +70,6 @@ function initPopUps() {
 					default:
 						break;
 				}
-			});
-		});
-	});
-}
-
-function sideMenu() {
-	const sideMenu = document.querySelector(".pop-up_side-menu");
-	const closeBtns = sideMenu.querySelectorAll('[data-popup-close="cart"]');
-	const openBtns = document.querySelectorAll('[data-popup-taget="cart"]');
-
-	openBtns.forEach((btn) => {
-		btn.addEventListener("click", () => {
-			blockScroll();
-			showPopUp(sideMenu);
-			gsap.fromTo(
-				".cart",
-				{
-					x: "100%",
-					opacity: 0,
-				},
-				{
-					x: 0,
-					opacity: 1,
-				},
-			);
-		});
-	});
-
-	closeBtns.forEach((btn) => {
-		btn.addEventListener("click", () => {
-			hidePopUp(sideMenu);
-			gsap.to(".cart", {
-				borderRadius: "50% 0 0 50%",
-				x: "100%",
-				opacity: 0,
-			});
-		});
-	});
-}
-
-function pickSize() {
-	const pickSize = document.querySelector("[data-popup='pick-size']");
-	const closeBtns = pickSize.querySelectorAll(
-		'[data-popup-close="pick-size"]',
-	);
-	const openBtns = document.querySelectorAll('[data-popup-open="pick-size"]');
-
-	openBtns.forEach((btn) => {
-		btn.addEventListener("click", (e) => {
-			e.preventDefault();
-			blockScroll();
-			showPopUp(pickSize);
-			gsap.fromTo(
-				".pick-size",
-				{
-					y: "100%",
-					opacity: 0,
-				},
-				{
-					y: 0,
-					opacity: 1,
-				},
-			);
-		});
-	});
-
-	closeBtns.forEach((btn) => {
-		btn.addEventListener("click", () => {
-			hidePopUp(pickSize);
-			gsap.to(".pick-size", {
-				y: "100%",
-				opacity: 0,
 			});
 		});
 	});
